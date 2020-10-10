1a5c5454a372a1c512c604ff7fa57575c863b621
sharifimehr.mahdi <sharifimehr.mahdi@gmail.com>
2020-10-10 12:55:25 +0330

    commit

--- App.js
+++ App.js
@@ -9,23 +9,86 @@ import {
   View,
   Text,
 } from 'react-native';
+import NetInfo from "@react-native-community/netinfo";
 
 class App extends Component {
+
+  NetInfoSubscribtion = null;
+
   constructor(props) {
     super(props);
     this.state = {
+      connection_status: false,
+      connection_type: null,
+      connection_net_reachable: false,
+      connection_wifi_enabled: false,
+      connection_details: null,
     }
   }
 
   componentDidMount() {
 
+    this.NetInfoSubscribtion = NetInfo.addEventListener(
+      this._handleConnectivityChange,
+    );
+
+  }
+
+  componentWillUnmount() {
+    this.NetInfoSubscribtion && this.NetInfoSubscribtion();
+  }
+
+  _handleConnectivityChange = (state) => {
+    this.setState({
+      connection_status: state.isConnected,
+      connection_type: state.type,
+      connection_net_reachable: state.isInternetReachable,
+      connection_wifi_enabled: state.isWifiEnabled,
+      connection_details: state.details,
+    })
   }
 
   render() {
     return (
       <View style={styles.body}>
         <Text style={styles.buttonText}>
-          
+          Connection Status : {this.state.connection_status ? 'Connected' : 'Disconnected'}
+        </Text>
+        <Text style={styles.buttonText}>
+          Connection Type : {this.state.connection_type}
+        </Text>
+        <Text style={styles.buttonText}>
+          Internet Reachable : {this.state.connection_net_reachable ? 'YES' : 'NO'}
+        </Text>
+        <Text style={styles.buttonText}>
+          Wifi Enabled : {this.state.connection_wifi_enabled ? 'YES' : 'NO'}
+        </Text>
+        <Text style={styles.buttonText}>
+          Connection Details : {'\n'}
+          {this.state.connection_type == 'wifi' ?
+            (this.state.connection_details.isConnectionExpensive ? 'Connection Expensive: YES' : 'Connection Expensive: NO')
+              + '\n'
+              + 'ssid: ' + this.state.connection_details.ssid
+              + '\n'
+              + 'bssid: ' + this.state.connection_details.bssid
+              + '\n'
+              + 'strength: ' + this.state.connection_details.strength
+              + '\n'
+              + 'ipAddress: ' + this.state.connection_details.ipAddress
+              + '\n'
+              + 'subnet: ' + this.state.connection_details.subnet
+              + '\n'
+              + 'frequency: ' + this.state.connection_details.frequency
+            :
+            this.state.connection_type == 'cellular' ?
+              (this.state.connection_details.isConnectionExpensive ? 'Connection Expensive: YES' : 'Connection Expensive: NO')
+                + '\n'
+                + 'cellularGeneration: ' + this.state.connection_details.cellularGeneration
+                +'\n'
+                +'carrier: '+this.state.connection_details.carrier
+              :
+              '---'
+          }
         </Text>
       </View>
     );
